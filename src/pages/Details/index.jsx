00c1b11510946132e594b418a470f82b12765c26
ronaldo <ronaldo.dsantos@gmail.com>
2024-07-03 01:22:40 -0300

    Created Pages Details and Component Button

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,9 +1,13 @@
 import { Container } from "./styles"
+import { Button } from "../../components/Button"
 
 export function Details(){
   return(
     <Container>
       <h1>Hello World!</h1>
+
+      <Button title="Entrar" loading/>
+      <Button title="Sair"/>
     </Container>
 
   )
---
+++ src/styles/theme.js
@@ -0,0 +1,13 @@
+export default {
+  COLORS: {
+    BACKGROUND_900: "#1C1B1E",
+    BACKGROUND_800: "#312E38",
+    BACKGROUND_700: "rgba(255, 133, 155, 0.05",
+
+    WHITE: "#F4EDE8",
+    PINK: "#FF859B",
+
+    GRAY_100: "#999591",
+    GRAY_300: "#948F99",
+  }
+} 
