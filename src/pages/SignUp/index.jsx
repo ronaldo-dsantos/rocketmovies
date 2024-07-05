c939c1e28a4dca32ff29787fb4e5f7a84dc03edf
ronaldo <ronaldo.dsantos@gmail.com>
2024-07-04 23:18:11 -0300

    Created Page SignUp

---
+++ src/pages/SignUp/index.jsx
@@ -0,0 +1,46 @@
+import { Container, Form, Background } from "./styles";
+import { FiUser, FiMail, FiLock, FiArrowLeft } from "react-icons/fi";
+
+import { Input } from "../../components/Input"
+import { Button } from "../../components/Button"
+
+export function SignUp(){
+  return(
+    <Container>
+      <Form>
+        <h1>RocketMovies</h1>
+        <p>Aplicação para acompanhar tudo que assistir.</p>
+        
+        <h2>Crie sua conta</h2>
+
+        <Input
+          placeholder="Nome"
+          type="text"
+          icon={FiUser}  
+        />
+
+        <Input
+          placeholder="E-mail"
+          type="text"
+          icon={FiMail}  
+        />
+
+        <Input
+          placeholder="Senha"
+          type="password"
+          icon={FiLock}  
+        />
+
+        <Button title="Cadastrar"/>
+
+        <a href="#">
+          <FiArrowLeft />
+          Voltar para o login
+        </a>
+      </Form>
+
+      <Background />
+
+    </Container>
+  )
+}
