fc9eefeaa92927e7f7f5064d60c0bff59b718237
ronaldo <ronaldo.dsantos@gmail.com>
2024-08-16 17:06:42 -0300

    Created user registration functionality

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,38 +1,76 @@
+import { useState } from "react";
 import { Container, Form, Background } from "./styles";
 import { FiUser, FiMail, FiLock, FiArrowLeft } from "react-icons/fi";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
+import { api } from "../../services/api"
 
 import { Input } from "../../components/Input"
 import { Button } from "../../components/Button"
 
-export function SignUp(){
-  return(
+export function SignUp() {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const navigate = useNavigate()
+
+  function handleSignUp() {
+    if (!name || !email || !password) {
+      return alert("Preencha todos os campos!")
+    }
+
+    api.post("/users", { name, email, password })
+      .then(() => {
+        alert("Usuário cadastrado com sucesso!")
+        navigate("/")
+      })
+      .catch(error => {
+        if (error.response) {
+          alert(error.response.data.message)
+        } else {
+          alert("Não foi possível cadastrar")
+        }
+      })
+
+
+
+
+  }
+
+  return (
     <Container>
       <Form>
         <h1>RocketMovies</h1>
         <p>Aplicação para acompanhar tudo que assistir.</p>
-        
+
         <h2>Crie sua conta</h2>
 
         <Input
           placeholder="Nome"
           type="text"
-          icon={FiUser}  
+          icon={FiUser}
+          onChange={e => setName(e.target.value)}
         />
 
         <Input
           placeholder="E-mail"
           type="text"
-          icon={FiMail}  
+          icon={FiMail}
+          onChange={e => setEmail(e.target.value)}
         />
 
         <Input
           placeholder="Senha"
           type="password"
-          icon={FiLock}  
+          icon={FiLock}
+          onChange={e => setPassword(e.target.value)}
         />
 
-        <Button title="Cadastrar"/>
+        <Button
+          title="Cadastrar"
+          onClick={handleSignUp}
+        />
 
         <Link to="/">
           <FiArrowLeft />
@@ -45,3 +83,4 @@ export function SignUp(){
     </Container>
   )
 }
+
