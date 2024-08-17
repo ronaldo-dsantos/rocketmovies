1786cb8530eb56ee511db90d5f3319ca4a1dd990
ronaldo <ronaldo.dsantos@gmail.com>
2024-08-16 22:31:02 -0300

    Created hooks signin

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,32 +1,46 @@
+import { useState } from "react";
 import { Container, Form, Background } from "./styles";
 import { FiMail, FiLock } from "react-icons/fi";
 import { Link } from "react-router-dom";
 
+import { useAuth } from "../../hooks/auth";
+
 import { Input } from "../../components/Input"
 import { Button } from "../../components/Button"
 
-export function SignIn(){
-  return(
+export function SignIn() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const { SignIn } = useAuth()
+
+  function handleSignIn() {
+    SignIn({ email, password })
+  }
+
+  return (
     <Container>
       <Form>
         <h1>RocketMovies</h1>
         <p>Aplicação para acompanhar tudo que assistir.</p>
-        
+
         <h2>Faça seu login</h2>
 
         <Input
           placeholder="E-mail"
           type="text"
-          icon={FiMail}  
+          icon={FiMail}
+          onChange={e => setEmail(e.target.value)}
         />
 
         <Input
           placeholder="Senha"
           type="password"
-          icon={FiLock}  
+          icon={FiLock}
+          onChange={e => setPassword(e.target.value)}
         />
 
-        <Button title="Entrar"/>
+        <Button title="Entrar" onClick={handleSignIn} />
 
         <Link to="/register">
           Criar conta
