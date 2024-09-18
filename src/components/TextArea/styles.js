75bf9f731b2390fcdd10fb4169351b94890a0020
ronaldo <ronaldo.dsantos@gmail.com>
2024-09-17 23:12:28 -0300

    Textarea overflow customization

--- src/components/TextArea/styles.js
+++ src/components/TextArea/styles.js
@@ -3,6 +3,21 @@ import styled from "styled-components";
 export const Container = styled.textarea`
   width: 100%;
   height: 274px;
+
+  overflow-y: auto;
+
+  &::-webkit-scrollbar {
+    width: 8px;      
+  }
+
+  &::-webkit-scrollbar-track {
+    background: none;      
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background: ${({ theme }) => theme.COLORS.PINK}; 
+    border-radius: 8px;      
+  }
   
   padding: 20px 16px;
   border-radius: 10px;
